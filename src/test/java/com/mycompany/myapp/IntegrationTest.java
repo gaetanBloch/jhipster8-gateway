21eb42ce5b6a9fe81b18968960b46f9fa3378825
Gaëtan Bloch <gaetan.bloch@keyconsulting.fr>
2023-12-08 08:46:44 +0100

    Initial version of jhipstergateway generated by generator-jhipster@8.0.0

---
+++ src/test/java/com/mycompany/myapp/IntegrationTest.java
@@ -0,0 +1,26 @@
+package com.mycompany.myapp;
+
+import com.mycompany.myapp.config.AsyncSyncConfiguration;
+import com.mycompany.myapp.config.EmbeddedSQL;
+import com.mycompany.myapp.config.TestSecurityConfiguration;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+/**
+ * Base composite annotation for integration tests.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(classes = { JhipstergatewayApp.class, AsyncSyncConfiguration.class, TestSecurityConfiguration.class })
+@EmbeddedSQL
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+public @interface IntegrationTest {
+    // 5s is Spring's default https://github.com/spring-projects/spring-framework/blob/main/spring-test/src/main/java/org/springframework/test/web/reactive/server/DefaultWebTestClient.java#L106
+    String DEFAULT_TIMEOUT = "PT5S";
+
+    String DEFAULT_ENTITY_TIMEOUT = "PT5S";
+}
